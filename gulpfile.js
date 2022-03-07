d356f869df2b3d75e3ebd5f2eb33f6e92bdeb658
huylesitdn <mrsjiodev@gmail.com>
2022-03-08 00:21:32 +0700

    update code, universal page

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+const gulp        = require('gulp');
+const fileinclude = require('gulp-file-include');
+const server = require('browser-sync').create();
+const { watch, series } = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+var concat = require('gulp-concat');
+
+const paths = {
+  scripts: {
+    src: './src/',
+    dest: './build/'
+  },
+  scss: {
+    src: ['./src/assets/scss/*.scss', './src/assets/scss/**/*.scss'],
+    exclude: '!./src/assets/scss/**/*.scss'
+  },
+  js: {
+    dest_dir: './assets/js'
+  },
+  css: {
+    dest_dir: './assets/css'
+  },
+  i18n: {
+    src: './src/assets/i18n/*.json',
+    dest_dir: './assets/i18n'
+  },
+};
+
+// Reload Server
+async function reload() {
+  server.reload();
+}
+
+// Sass compiler
+async function compileScss() {
+  gulp.src(paths.scss.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.css.dest_dir));
+}
+
+// JS vendor compiler
+async function compileVendorJS() {
+  gulp.src('./src/assets/js/vendor.js')
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(paths.js.dest_dir));
+}
+async function compileJS() {
+  gulp.src('./src/assets/js/index.js')
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest(paths.js.dest_dir));
+}
+async function compileI18n() {
+  gulp.src([paths.i18n.src])
+    .pipe(gulp.dest(paths.i18n.dest_dir));
+}
+
+// Copy assets after build
+async function copyAssets() {
+  gulp.src(['assets/**/*'])
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// Build files html and reload server
+async function buildAndReload() {
+  await includeHTML();
+  await copyAssets();
+  reload();
+}
+
+async function includeHTML(){
+  return gulp.src([
+    './src/pages/*.html',
+    './src/pages/**/*.html',
+    '!./src/components/**/*.html', // ignore
+    ])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+exports.includeHTML = includeHTML;
+
+exports.default = async function() {
+  // Init serve files from the build folder
+  server.init({
+    server: {
+      baseDir: paths.scripts.dest
+    }
+  });
+  // Build and reload at the first time
+  buildAndReload();
+  // Watch Sass task
+  watch(paths.scss.src,  series(compileScss));
+  // watch i18n
+  watch([paths.i18n.src],  series(compileI18n));
+  // Watch js task
+  watch('./src/assets/js/vendor.js',  series(compileVendorJS));
+  watch(['./src/assets/js/index.js'],  series(compileJS));
+  // Watch task
+  watch(["./src/pages/*.html", "assets/**/*"], series(buildAndReload));
+};
---
+++ src/assets/js/index.js
@@ -0,0 +1,24 @@
+// Translator
+
+const LANGUAGES = {
+  EN: 'en',
+  ZH: 'zh'
+}
+
+var translator = new Translator({
+  defaultLanguage: 'en',
+  detectLanguage: true,
+  selector: '[data-i18n]',
+  debug: false,
+  registerGlobally: '__',
+  persist: false,
+  persistKey: 'preferred_language',
+  filesLocation: '/i18n',
+});
+
+translator.fetch([LANGUAGES.EN, LANGUAGES.ZH]).then(() => {
+  // -> Translations are ready...
+  translator.translatePageTo(LANGUAGES.EN);
+});
+
+console.log('--- index.js')
