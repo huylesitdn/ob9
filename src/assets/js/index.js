53036f48c047867d8246341cde875e6df9c85933
huylesitdn <mrsjiodev@gmail.com>
2022-03-17 11:54:51 +0700

    transaction history

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -159,8 +159,8 @@ $(function () {
  *
  */
 
-const selectPromotionModalElm = $("#selectPromotionModal")
-if(selectPromotionModalElm.length > 0) {
+const selectPromotionModalElm = $("#selectPromotionModal");
+if (selectPromotionModalElm.length > 0) {
   var selectPromotionModal = new bootstrap.Modal(selectPromotionModalElm, {});
 }
 $(".select-promotion__items").on("click", function (e) {
@@ -168,20 +168,13 @@ $(".select-promotion__items").on("click", function (e) {
     selectPromotionModal.hide();
     $(".deposit-amount__summary").removeClass("d-none");
     $(".deposit-amount__action .btn-submit").attr("disabled", false);
-    $("#select-promotion-placeholder").text(
-      "Welcome Bonus up to 180%"
-    );
-    $("#select-promotion-placeholder").addClass(
-      "fw-bold"
-    );
-    $("#select-promotion-placeholder").css(
-      "color",
-      "#000"
-    );
+    $("#select-promotion-placeholder").text("Welcome Bonus up to 180%");
+    $("#select-promotion-placeholder").addClass("fw-bold");
+    $("#select-promotion-placeholder").css("color", "#000");
   }, 500);
 });
 
-const selectBankModalElm = $("#selectBankModal")
+const selectBankModalElm = $("#selectBankModal");
 if (selectBankModalElm.length > 0) {
   var selectBankModal = new bootstrap.Modal(selectBankModalElm, {});
 }
@@ -198,16 +191,20 @@ $(".select-bank-modal__items").on("click", function (e) {
 $(".add-bank-account .select-bank-modal__items").on("click", function (e) {
   setTimeout(() => {
     selectBankModal.hide();
-    const bank_input = $('.add-bank-account .add-bank-account__content__input__select-bank__input__placeholder');
-    bank_input.html('MAYBANK');
-    bank_input.addClass('fw-bold');
-    const submit_btn = $('.add-bank-account .add-bank-account__content__submit .btn');
+    const bank_input = $(
+      ".add-bank-account .add-bank-account__content__input__select-bank__input__placeholder"
+    );
+    bank_input.html("MAYBANK");
+    bank_input.addClass("fw-bold");
+    const submit_btn = $(
+      ".add-bank-account .add-bank-account__content__submit .btn"
+    );
     submit_btn.removeClass("disabled");
     // submit_btn.prop("disabled", false);
   }, 500);
 });
 
-const successModalElm = $("#successModal")
+const successModalElm = $("#successModal");
 if (successModalElm.length > 0) {
   var successModal = new bootstrap.Modal(successModalElm, {});
 }
@@ -215,51 +212,80 @@ $("#payment-gateway .btn-submit").on("click", function (e) {
   successModal.show();
 });
 
-const transferConfirmModalElm = $("#transferConfirmModal")
+const transferConfirmModalElm = $("#transferConfirmModal");
 if (transferConfirmModalElm.length > 0) {
   var transferConfirmModal = new bootstrap.Modal(transferConfirmModalElm, {});
 }
 $("#autoTransferCheck").on("click", function (e) {
-  const isCheck = $(this).is(':checked');
-  if(!isCheck) {
+  const isCheck = $(this).is(":checked");
+  if (!isCheck) {
     e.preventDefault();
-    transferConfirmModal.show()
+    transferConfirmModal.show();
   } else {
-    $(".transfer .transfer__content__auto-switch-off").addClass('d-none');
-    $(".transfer .transfer__content__action").addClass('d-none');
+    $(".transfer .transfer__content__auto-switch-off").addClass("d-none");
+    $(".transfer .transfer__content__action").addClass("d-none");
   }
 });
 $("#transferConfirmModal .btn-confirm").on("click", function (e) {
-  const isCheck = $('#autoTransferCheck').is(':checked');
-  $("#autoTransferCheck").prop('checked', !isCheck);
+  const isCheck = $("#autoTransferCheck").is(":checked");
+  $("#autoTransferCheck").prop("checked", !isCheck);
   transferConfirmModal.hide();
-  $(".transfer .transfer__content__auto-switch-off").removeClass('d-none');
-  $(".transfer .transfer__content__action").removeClass('d-none');
+  $(".transfer .transfer__content__auto-switch-off").removeClass("d-none");
+  $(".transfer .transfer__content__action").removeClass("d-none");
 });
 
-const chooseWalletModalElm = $("#chooseWalletModal")
+const chooseWalletModalElm = $("#chooseWalletModal");
 if (chooseWalletModalElm.length > 0) {
   var chooseWalletModal = new bootstrap.Modal(chooseWalletModalElm, {});
 }
-$("#chooseWalletModal .choose-modal__items input[name=choose-modal-radio]").on("change", function (e) {
-  const current_value = $('#chooseWalletModal .choose-modal__items input[name=choose-modal-radio]:checked').val()
-  setTimeout(() => {
-    const attach_new_elem = current_value.split('_')
-    $('#auto-switch-off--left').html(attach_new_elem[0]);
-    $('#auto-switch-off--right').html(attach_new_elem[1]);
-    chooseWalletModal.hide()
-  }, 500);
-});
+$("#chooseWalletModal .choose-modal__items input[name=choose-modal-radio]").on(
+  "change",
+  function (e) {
+    const current_value = $(
+      "#chooseWalletModal .choose-modal__items input[name=choose-modal-radio]:checked"
+    ).val();
+    setTimeout(() => {
+      const attach_new_elem = current_value.split("_");
+      $("#auto-switch-off--left").html(attach_new_elem[0]);
+      $("#auto-switch-off--right").html(attach_new_elem[1]);
+      chooseWalletModal.hide();
+    }, 500);
+  }
+);
 
-if($('.transaction-history-dropdown').length > 0) {
-  $('.transaction-history-dropdown').each(function( index ) {
-    this.addEventListener('hidden.bs.dropdown', function () {
-      $('.transaction-history').removeClass('backdrop')
-    })
-    this.addEventListener('shown.bs.dropdown', function () {
-      $('.transaction-history').addClass('backdrop')
-    })
+if ($(".transaction-history-dropdown").length > 0) {
+  $(".transaction-history-dropdown").each(function (index) {
+    this.addEventListener("hidden.bs.dropdown", function () {
+      $(".transaction-history").removeClass("backdrop");
+    });
+    this.addEventListener("shown.bs.dropdown", function () {
+      $(".transaction-history").addClass("backdrop");
+    });
   });
 }
+
+$(".dropdown-menu").on("click", function (e) {
+  e.stopPropagation();
+});
+
+/**
+ * mobiscroll
+ *
+ **/
+
+mobiscroll.setOptions({
+  theme: "ios",
+  themeVariant: "light",
+});
+
+$(function () {
+  $(".date-picker")
+    .mobiscroll()
+    .datepicker({
+      controls: ["date"],
+      touchUi: true,
+      display: 'bottom',
+    });
+});
 
 console.log("--- index.jsaaa");
